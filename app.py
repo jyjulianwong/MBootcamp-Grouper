87a051b89e138633a9afe7a49cadc783b3b740b2
Julian Wong <julianwong2000@gmail.com>
2022-11-22 22:19:58 +0000

    Upload sample data from MBootcamp 2022

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def root():
+    return None
 
 
 if __name__ == '__main__':
